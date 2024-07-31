b28f29c1bd9308c584efc1dac44513ac61b3a598
simranmaurya123 <simran.maurya.932@gmail.com>
2024-07-31 21:15:05 +0530

    website type and protocol

---
+++ web.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class web{
+    public static void main(String[] args) {
+       System.out.println("Enter a website url:  ");
+       Scanner sc = new Scanner(System.in);
+       String url=sc.nextLine();
+
+       String protocol=url.substring(0, url.indexOf(":"));
+        
+    if(protocol.equals("http"))
+        System.out.println("Hypertext Transfer Protocol");
+    else if(protocol.equals("ftp"))
+        System.out.println("File Transfer Protocol");
+        
+        
+    String ext=url.substring(url.lastIndexOf(".")+1);
+        
+    if(ext.equals("com"))
+        System.out.println("Commercial");
+    else if(ext.equals("org"))
+        System.out.println("Organisation");
+    else if(ext.equals("net"))
+        System.out.println("Network");
+    }
+}
