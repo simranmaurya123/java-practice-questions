96fdc747635a8ae05674c8bb616d6e31d9f3944d
simranmaurya123 <simran.maurya.932@gmail.com>
2024-08-21 11:12:08 +0530

    Fibonacci Series

---
+++ fibonacci_series.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fibonacci_series{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of terms: ");
+        int n=sc.nextInt();
+ 
+
+        int a=0,b=1,c;
+        System.out.print(a+","+b+",");
+        for(int i=0;i<n-2;i++){
+            c=a+b;
+            System.out.print(c+",");
+            a=b;
+            b=c;
+
+        } 
+
+        }
+    }
